84d1ed4b048d31014b428f4eb4ee21c62ac221cd
renato_sans <renato@datacopy.com.br>
2022-07-30 04:44:28 -0300

    Ajustes

--- config/db.js
+++ config/db.js
@@ -1,17 +1,22 @@
 const { PrismaClient } = require('@prisma/client');
 
 
-// dados para conexão com banco de dados serverless
-const host     = 'r0e93010lf0s.us-east-3.psdb.cloud';
-const username = Buffer.from('YmhiYzBtaW13dTc1', 'base64').toString('ascii');
-const password = Buffer.from('cHNjYWxlX3B3X0F5ZHFjcG1nSnROSFZpYnUwX3NfeDFjaWp0Q0V3eGZvVERpNVRXakZJVHM=', 'base64').toString('ascii');
+// dados da conexão
+const host     = 'localhost';
+const username = 'root';
+const password = 'p@ssw0rd';
 const port     = 3306
-const database = 'hercules8'
+const database = 'h8'
+const ssl      = false
 const setSSL   = 'sslaccept=strict&sslmode=require'
 
 
 // DATABASE_URL="mysql://root:p@ssw0rd@localhost:3306/h8"
-const url = `mysql://${username}:${password}@${host}:${port}/${database}?${setSSL}`;
+let url = `mysql://${username}:${password}@${host}:${port}/${database}`;
+if (ssl) {
+    url = url + `?${setSSL}`;
+}
+
 const prisma = new PrismaClient({datasources: { db: { url: url } } })
 
 module.exports = prisma
