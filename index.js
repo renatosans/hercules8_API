433a8c6fafcbfea8587233b182e6ce20857d2191
renato_sans <renato@datacopy.com.br>
2022-08-05 14:12:05 -0300

    Correção de bugs

--- index.js
+++ index.js
@@ -17,5 +17,6 @@ app.use(cors())
 // createRouter(app, { directory: apiDirectory, additionalMethods: null } )
 app.use(nextApi({ base: '/api', directory: 'api', options: {caseSensitive: false} }))
 
+
 // inicia a API escutando na porta 3000
 app.listen(port, () => console.log('Express escutando chamadas na porta ' + port));
